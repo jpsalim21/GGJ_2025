faf2158f0bf73995069a2fc4e9ca89c4bbfb90a3
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2025-01-25 11:34:18 -0300

    Cria bolha pulo

--- Scripts/BolhaPulo.gd
+++ Scripts/BolhaPulo.gd
@@ -1,10 +1,12 @@
 extends BolhaBase
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var forcaImpulso : float
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func encostou(body: Node2D) -> void:
+	var player = body as Player
+	if not player:
+		return
+	
+	player.apply_impulse(Vector2(0, -forcaImpulso))
+	
+	destruir()
