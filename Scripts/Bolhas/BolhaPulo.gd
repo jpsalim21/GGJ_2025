c7e5d4f6121abbaf843df54f50f1d9228a2aaf14
MarcelodMelo <marcelodmeloamorim@gmail.com>
2025-01-25 20:35:54 -0300

    LevelDesing incial jogo 1

--- Scripts/Bolhas/BolhaPulo.gd
+++ Scripts/Bolhas/BolhaPulo.gd
@@ -3,5 +3,5 @@ extends BolhaBase
 @export var forcaImpulso : float = 500.0
 
 func encostou(player : Player) -> void:
-	
+	player.linear_velocity = Vector2(player.linear_velocity.x, 0)
 	player.apply_impulse(Vector2(0, -forcaImpulso))
--- Scripts/Bolhas/BolhaRicochete.gd
+++ Scripts/Bolhas/BolhaRicochete.gd
@@ -3,8 +3,8 @@ extends BolhaBase
 @export var forcaImpulso : float = 500.0
 
 func encostou(player: Player) -> void:
-	var dir = sign(player.linear_velocity.x)
 	
 	player.angular_velocity = 0
-	player.linear_velocity = Vector2.ZERO
-	player.apply_impulse(Vector2(dir * -forcaImpulso, 0))
+	var dir = player.linear_velocity
+	player.linear_velocity = Vector2(0, 0)
+	player.apply_impulse(dir.normalized() * forcaImpulso * Vector2(-1, 1))
